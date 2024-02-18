db8001a6af61b8219d2113ab59f200e0cd6db65f
Sihoiba <dominic.decesare+github@gmail.com>
2024-02-18 19:28:03 +0000

    Smuggler ammo trait

--- inmate_skills.lua
+++ inmate_skills.lua
@@ -60,6 +60,9 @@ register_blueprint "ktrait_mule"
         full   = "You are the go to guy to move contraband around callisto!\n\n{!LEVEL 1} - {!+1} inventory slot, reveal elevators\n{!LEVEL 2} - {!+3} inventory slots\n{!LEVEL 3} - {!+5} inventory slots, reveal lost boxes",
         abbr   = "Mul",
     },
+    attributes = {
+        level   = 1,
+    },
     callbacks = {
         on_activate = [=[
             function( self, entity )
@@ -93,5 +96,76 @@ register_blueprint "ktrait_mule"
                 end
             end
         ]=],
+    },
+}
+
+register_blueprint "ktrait_smuggler"
+{
+    blueprint = "trait",
+    text = {
+        name   = "Smuggler",
+        desc   = "Find ammo in descructable environment objects",
+        full   = "You know where the black market stashes items!\n\n{!LEVEL 1} - small amount of ammo in every object for current weapon, except grenades and rockets\n{!LEVEL 2} - medium amount of ammo, grenades can be found \n{!LEVEL 3} - large amount of ammo, rockets can be found",
+        abbr   = "Smg",
+    },
+    attributes = {
+        level   = 1,
+    },
+    callbacks = {
+        on_activate = [=[
+            function( self, entity )
+                gtk.upgrade_trait( entity, "ktrait_smuggler" )
+            end
+        ]=],
+        on_kill = [[
+            function ( self, entity, target, weapon, gibbed, coord )
+                local level = world:get_level()
+                local tlevel = self.attributes.level or 0
+                local wep = entity:get_weapon()
+                if target and wep and wep.weapon and not (target.data and target.data.ai) and not target.hazard and wep.weapon.type ~= world:hash("melee") and wep.clip and wep.clip.ammo then
+
+                    local ammos  =
+                    {
+                        [world:hash("ammo_9mm")]     = { id = "ammo_9mm", },
+                        [world:hash("ammo_shells")]  = { id = "ammo_shells",},
+                        [world:hash("ammo_762")]     = { id = "ammo_762", },
+                        [world:hash("ammo_44")]      = { id = "ammo_44", },
+                        [world:hash("ammo_40")]      = { id = "ammo_40", },
+                        [world:hash("ammo_cells")]   = { id = "ammo_cells", },
+                        [world:hash("ammo_rockets")] = { id = "ammo_rockets", },
+                    }
+
+                    local ammo = ammos[wep.clip.ammo]
+                    local grenades = "ammo_40"
+                    local rockets = "ammo_rockets"
+
+                    if tlevel == 1 and ammo.id ~= grenades and ammo.id ~= rockets then
+                        local e = world:create_entity( ammo.id )
+                        e.stack.amount = 1 + math.random(2)
+                        level:drop_entity( e, coord )
+                    elseif tlevel == 2 and ammo.id == grenades then
+                        local e = world:create_entity( ammo.id )
+                        e.stack.amount = 1 + math.random(2)
+                        level:drop_entity( e, coord )
+                    elseif tlevel == 2 and ammo.id ~= grenades and ammo.id ~= rockets then
+                        local e = world:create_entity( ammo.id )
+                        e.stack.amount = 5 + math.random(5)
+                        level:drop_entity( e, coord )
+                    elseif tlevel == 3 and ammo.id == grenades then
+                        local e = world:create_entity( ammo.id )
+                        e.stack.amount = 3 + math.random(3)
+                        level:drop_entity( e, coord )
+                    elseif tlevel == 3 and ammo.id == rockets then
+                        local e = world:create_entity( ammo.id )
+                        e.stack.amount = 1 + math.random(2)
+                        level:drop_entity( e, coord )
+                    elseif tlevel == 3 and ammo.id ~= grenades and ammo.id ~= rockets then
+                        local e = world:create_entity( ammo.id )
+                        e.stack.amount = 10 + math.random(10)
+                        level:drop_entity( e, coord )
+                    end
+                end
+            end
+        ]],
     },
 }
